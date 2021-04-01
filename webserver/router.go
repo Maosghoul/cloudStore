8d6025caba0a9e20b9113104146fd0a78970206c
Maosghoul <38350093+Maosghoul@users.noreply.github.com>
2021-04-01 22:34:50 +0800

    Create router.go

---
+++ webserver/router.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/cloudStore/handler"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func register(r *gin.Engine) {
+	customizeRegister(r)
+	r.GET("/ping", handler.Ping)
+}
+
+func customizeRegister(r *gin.Engine) {
+	v := r.Group("/cs")
+	// people manage
+	v.POST("/login", handler.Login)
+	v.POST("/register", handler.Register)
+	v.POST("/modify", handler.Modify)
+
+	// file manage
+	v.POST("/upload_file", handler.UploadFile)
+	v.POST("/delete_file", handler.DeleteFile)
+	v.POST("/list_file", handler.ListFile)
+}
+
+func loadHTML(r *gin.Engine) {
+	r.GET("/index", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "云盘",
+		})
+	})
+	r.GET("/login", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "login.html", gin.H{
+			"title": "登录",
+		})
+	})
+	r.GET("/reg", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "reg.html", gin.H{
+			"title": "注册",
+		})
+	})
+}
