055d7d50e3cb52a83a4a55fd370a049e63cb6983
Maosghoul <38350093+Maosghoul@users.noreply.github.com>
2021-04-01 22:30:41 +0800

    Create config.go

---
+++ webserver/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
+var Conf Config
+
+type Config struct {
+	MysqlConfig MysqlConfig `yaml:"MysqlConfig"`
+}
+
+type MysqlConfig struct {
+	DBName   string `yaml:"DBName"`
+	UserName string `yaml:"UserName"`
+	PassWord string `yaml:"PassWord"`
+	Host     string `yaml:"Host"`
+	Port     string `yaml:"Port"`
+}
+
+func Init() error {
+	yamlFile, err := ioutil.ReadFile("conf/cloudStore.yaml")
+	log.Printf("config init success:%s\n", string(yamlFile))
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(yamlFile, &Conf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
