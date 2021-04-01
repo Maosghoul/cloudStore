9df57387a23f833e096a66cadf1ef8f8d3ff3384
Maosghoul <38350093+Maosghoul@users.noreply.github.com>
2021-04-01 22:39:49 +0800

    Create keshe.sql

---
+++ keshe.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `user` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+	`username` varchar(20) NOT NULL DEFAULT '' COMMENT 'username',
+	`password` varchar(100) NOT NULL DEFAULT '' COMMENT 'password',
+	`email` varchar(100) NOT NULL DEFAULT '' COMMENT 'email',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY(`username`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+INSERT INTO `user` (`id`,`username`,`password`,`email`) VALUES(?,?,?,?)
+
+SELECT * FROM `user` WHERE `username` = ?
+
+UPDATE `user` SET `password` = ? , `email` = ? WHERE `username` = ?
+
+CREATE TABLE `file` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+	`username` varchar(20) NOT NULL DEFAULT '' COMMENT 'username',
+	`filename` varchar(100) NOT NULL DEFAULT '' COMMENT 'filename',
+	`update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `per_file` (`username`,`filename`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+INSERT INTO `file` (`id`,`username`,`filename`,`update_time`) VALUES(0,?,?,?)
+
+UPDATE `file` SET `filename` = ?,`update_time` = ? WHERE `username` = ?
+
+DELETE from `file` WHERE `username` = ? AND `filename` = ?
+
+SELECT * FROM `file` WHERE `username` = ?
